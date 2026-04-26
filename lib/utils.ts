a91fe3761b7e52dcddcec27559e47d150f044750
Pato1531 <patriciomanganella@gmail.com>
2026-04-26 10:12:54 -0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,5 +7,5 @@ export const fmtCurrency = (n: number) =>
 export const fmtDate = (dateStr: string) =>
   new Date(dateStr).toLocaleDateString('es-AR', { day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' })
 
-export const fmtDateShort = (dateStr: string) =>
-  new Date(dateStr).toLocaleDateString('es-AR', { day: '2-digit', month: '2-digit' })
+export const fmtDate = (dateStr: string) =>
+  new Date(dateStr).toLocaleDateString('es-AR', { weekday: 'short', day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit' })
